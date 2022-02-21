6e81e47fc2ad75acfaf56e0ada6a579452dd9a57
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-02-21 14:19:24 +0100

    refacto to stateful widget to update correctly data

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,22 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+    // throw UnimplementedError();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   var questionIndex = 0;
 
   void answerQuestion() {
-    questionIndex = questionIndex + 1;
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
     print(questionIndex);
   }
 
